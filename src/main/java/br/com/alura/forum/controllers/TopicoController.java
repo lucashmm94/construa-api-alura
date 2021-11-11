e406cce159ef25097ca96c0e982f1a6cd7275846
Lucas Hermont <you@example.com>
2021-11-10 23:41:56 -0300

    Implementando ordenacao e paginacao

--- src/main/java/br/com/alura/forum/controllers/TopicoController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicoController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controllers;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -37,12 +41,16 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDTO> findAll(String nomeCurso) {
-		List<Topico> topicos;
+	public Page<TopicoDTO> findAll(@RequestParam(required = false)String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, size = 10) Pageable paginacao) {
+		
+		
+//		Pageable paginacao = PageRequest.of(pagina, qtd,Direction.DESC,ordenacao);
+		Page<Topico> topicos;
 		if (nomeCurso == null) {
-			topicos = repository.findAll();
+			topicos = repository.findAll(paginacao);
 		} else {
-			topicos = repository.findByCursoNome(nomeCurso);
+			topicos = repository.findByCursoNome(nomeCurso,paginacao);
 		}
 		return TopicoDTO.converter(topicos);
 	}
--- src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
@@ -1,7 +1,7 @@
 package br.com.alura.forum.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
@@ -10,5 +10,5 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	//Padrao findBy é o padrao, passa a propriedade curso que esta relacionado
 	//com titulo. Nome é atributo de curso, é so concatenar.
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso,Pageable paginacao);
 }
